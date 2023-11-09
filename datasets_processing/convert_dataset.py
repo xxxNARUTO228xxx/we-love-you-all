f278c0bf98eceda6e695e69d363b9dd227b8c6af
Anton Shiryaev <med.phisiker@gmail.com>
2023-11-09 07:56:00 +0000

    feat(datasets): convert datasets and transform masks to bboxes
    convert datasets and transform masks to bboxes

---
+++ datasets_processing/convert_dataset.py
@@ -0,0 +1,7 @@
+import datumaro as dm
+
+dataset = dm.Dataset.import_from("data/MockAttach", "cvat")
+dataset.transform("shapes_to_boxes")
+dataset.init_cache()
+dataset._data._media_type = dm.Image
+dataset.export("output_dataset", "cvat")
